39fc1d4b17da386305f924ebec980b4e38ce9c53
Bazsmagister <bosmer007@gmail.com>
2020-08-25 15:10:38 +0200

    user seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -21,21 +21,54 @@ class UsersTableSeeder extends Seeder
         // Let's make sure everyone has the same password and
         // let's hash it before the loop, or else our seeder
         // will be too slow.
-        $password = Hash::make('adminadmin');
+        // $password = Hash::make('adminadmin');
 
-        User::create([
-            'name' => 'adminadmin',
+
+        // User::create([
+        //     'name' => 'adminadmin',
+        //     'email' => 'admin@admin.com',
+        //     // 'password' => $password,
+        //     'password' => 'adminadmin'
+        // ]);
+
+
+
+        // // And now let's generate a few dozen users for our app:
+        // for ($i = 0; $i < 10; $i++) {
+        //     User::create([
+        //         'name' => $faker->name,
+        //         'email' => $faker->email,
+        //         'password' => $password,
+        //     ]);
+        // }
+
+        // $admi = User::create([
+        //     'name' => 'adminadmin',
+        //     'email' => 'admin@admin.com',
+        //     // 'password' => $password,
+        //     'password' =>  bcrypt('adminadmin'),
+        // ]);
+
+        // $authuse = User::create([
+        //     'name' => 'authuser',
+        //     'email' => 'authuser@authuser.com',
+        //     'password' =>  bcrypt('authuser'),
+        // ]);
+
+        $admin = factory(\App\User::class)->create([
+            'id'    => '1',
+            'name'  => 'Admin Adam',
             'email' => 'admin@admin.com',
-            'password' => $password,
+            'password' => bcrypt('adminadmin'),
+            // 'phone' => '0623232565321',
         ]);
 
-        // And now let's generate a few dozen users for our app:
-        for ($i = 0; $i < 10; $i++) {
-            User::create([
-                'name' => $faker->name,
-                'email' => $faker->email,
-                'password' => $password,
-            ]);
-        }
+        $authuser = factory(\App\User::class)->create([
+            'id'    => '2',
+            'name'  => 'Dr. Authenticated User',
+            'email' => 'authuser@authuser.com',
+            'password' => bcrypt('authuser'),
+            // 'phone' => '0623232565322',
+        ]);
     }
 }
